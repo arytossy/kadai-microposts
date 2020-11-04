862c9a988f65c2bdfbc5ce73c085984483e1d42c
arytossy <arytossy@gmail.com>
2020-11-04 02:56:31 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,19 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Microposts</a>
+        
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-nav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbar-nav">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#">Signup</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    
+</header>
